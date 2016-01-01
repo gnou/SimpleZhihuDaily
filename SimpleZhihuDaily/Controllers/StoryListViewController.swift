05bf0ea2c3579cdfdb054886753a0863617572ec
Malvin Cui <malvin.cui@gmail.com>
2016-01-01 15:57:00 +0800

    Create VC for story list First VC, will be used to display all the stories

---
+++ SimpleZhihuDaily/Controllers/StoryListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StoryListViewController.swift
+//  SimpleZhihuDaily
+//
+//  Created by CuiMingyu on 1/1/16.
+//  Copyright © 2016 CuiMingyu. All rights reserved.
+//
+
+import UIKit
+
+class StoryListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func configureTableView() {
+//        tableView
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
