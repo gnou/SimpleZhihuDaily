1b87d1f7b9436c87b6a028846affadba1c602d92
Malvin Cui <malvin.cui@gmail.com>
2017-03-09 21:55:25 +0800

    New cache manager

---
+++ SimpleZhihuDaily/Cache/CacheManager.swift
@@ -0,0 +1,16 @@
+//
+//  CacheManager.swift
+//  SimpleZhihuDaily
+//
+//  Created by Mingyu Cui on 09/03/2017.
+//  Copyright © 2017 CuiMingyu. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class CacheManager {
+    static let shared = CacheManager()
+    
+    
+}
